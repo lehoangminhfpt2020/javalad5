b91df0aee68e4b337adb8015cf58f570b558d8cb
minh <65264453+lehoangminhfpt2020@users.noreply.github.com>
2020-10-25 09:43:35 +0700

    luyện OPP Composition

---
+++ MyCircle.java
@@ -0,0 +1,8 @@
+package lab5;
+
+public class MyCircle {
+    private MyPoint center;
+    private int radius = 1;
+
+    public MyCircle(){}
+}
---
+++ testBook.java
@@ -0,0 +1,13 @@
+package lab5;
+
+public class testBook {
+    public static void main(String[] args) {
+        Author[] authors = new Author[2];
+        authors[0] = new Author("okem", "okanh@somewhere.com", 'm');
+        authors[1] = new Author("okanhem", "okem@nowhere.com", 'm');
+
+// Declare and allocate a Book instance
+//        Book javaDummy = new Book("Java for Dummy", authors, 19.99, 99);
+//        System.out.println(javaDummy);
+    }
+}
